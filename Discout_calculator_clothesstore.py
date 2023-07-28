b2b36e47cf9daf57eefe9ca3815897774b0a7c17
Renietz <140656877+Renietz@users.noreply.github.com>
2023-07-28 20:17:26 -0300

    01

---
+++ Discout_calculator_clothesstore.py
@@ -0,0 +1,28 @@
+# questão 2
+
+
+# VARIÁVEIS :
+
+
+valor_do_produto = float(input('Valor total da compra: R$ '))
+
+# PROGRAMA PRINCIPAL :
+
+if valor_do_produto <= 100:
+    print('Desconto aplicado: R${:.2f}'.format(valor_do_produto * .05))
+    print('Valor total com desconto: R${:.2f}' .format((valor_do_produto* .95)))
+
+elif valor_do_produto > 100 and valor_do_produto <= 200:
+    print('Desconto aplicado: R${:.2f}'.format(valor_do_produto * .10))
+    print('Valor total com desconto: R${:.2f}'.format((valor_do_produto * .90)))
+
+elif valor_do_produto >200 and valor_do_produto <=500:
+    print('Desconto aplicado: R${:.2f}'.format(valor_do_produto * .15))
+    print('Valor total com desconto: R${:.2f}'.format((valor_do_produto * .85)))
+
+else:
+    print('Desconto aplicado: R${:.2f}'.format(valor_do_produto * .20))
+    print('Valor total com desconto: R${:.2f}'.format((valor_do_produto * .80)))
+
+
+
